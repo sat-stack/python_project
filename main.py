d9149641312fd7dcd3b45de459cbbe85a253c39a
sat-stack <satabdi.patra23@gmail.com>
2021-04-08 12:24:29 +0530

    satabdi's first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("my name is satabdi")
+print(200)
+
+minutes = 24*60*60
+unit = 'sec'
+
+
+def days_to_units(number_of_days):
+    print(f"{number_of_days} days are {20 * minutes} {unit}")
+
+
+input("enter a number of days\n")
