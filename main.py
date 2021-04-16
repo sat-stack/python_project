b474714cf6b5af5efd24319c8c00f65700c0ce77
sat-stack <satabdi.patra23@gmail.com>
2021-04-17 00:12:14 +0530

    countdown_app_project

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-
-def days_to_units(number_of_days, conversion_unit):
-    if conversion_unit == "hours":
-        return f"{number_of_days} days are {number_of_days * 24} hours"
-    elif conversion_unit == "minutes":
-        return f"{number_of_days} days are {number_of_days * 24 * 60} minutes"
-    else:
-        return "unsupported unit"
-
-
-def validate_and_execute():
-    try:
-        user_input_number = int(days_and_unit_dictionary["days"])
-        if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
-            print(calculated_value)
-        elif user_input_number == 0:
-            print("please enter valid value")
-        else:
-            print("your number is negative, enter a positive number")
-
-    except ValueError:
-        print("enter numbers only, no text")
-
+import helper
 
 my_user_input = ""
 while my_user_input != "exit":
@@ -31,7 +8,20 @@ while my_user_input != "exit":
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
     print(days_and_unit_dictionary)
     print(type(days_and_unit_dictionary))
-    validate_and_execute()
+    helper.validate_and_execute(days_and_unit_dictionary)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
