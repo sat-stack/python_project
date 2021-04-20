fba0dbc4f9b0bd1b8a4083a9534a5f5ecf5e2531
sat-stack <satabdi.patra23@gmail.com>
2021-04-20 11:45:36 +0530

    automation_with_python_second_project

--- main_second.py
+++ main_second.py
@@ -1,18 +1,45 @@
-minutes = 24*60*60
-unit = 'sec'
+import openpyxl
 
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+product_list = inv_file["Sheet1"]
 
-def days_to_units(number_of_days):
-    return f"{number_of_days} days are {20 * minutes} {unit}"
+products_per_supplier = {}
+total_value_per_supplier = {}
+products_under_10_inv = {}
 
+for product_row in range(2, product_list.max_row + 1):
+    supplier_name = product_list.cell(product_row, 4).value
+    inventory = product_list.cell(product_row, 2).value
+    price = product_list.cell(product_row, 3).value
+    product_num = product_list.cell(product_row, 1).value
+    inventory_price = product_list.cell(product_row, 5)
 
-user_input = input("enter any number of days here and i will covert it into seconds!\n")
-user_input_number = int(user_input)
+    # calculation number of products per supplier
+    if supplier_name in products_per_supplier:
+        current_num_products = products_per_supplier.get(supplier_name)
+        products_per_supplier[supplier_name] = current_num_products + 1
+    else:
+        products_per_supplier[supplier_name] = 1
 
-calculated_value = days_to_units(user_input)
-print(calculated_value)
+# calculation of total value of inventory per supplier
+    if supplier_name in total_value_per_supplier:
+        current_total_value = total_value_per_supplier.get(supplier_name)
+        total_value_per_supplier[supplier_name] = current_total_value + inventory * price
+    else:
+        total_value_per_supplier[supplier_name] = inventory * price
 
+    # logic products with inventory less than 10
+    if inventory < 10:
+        products_under_10_inv[int(product_num)] = int(inventory)
 
+    # add value for total inventory price
+    inventory_price.value = inventory * price
 
+print(products_per_supplier)
+print(total_value_per_supplier)
+print(products_under_10_inv)
+
+    # save the file
+inv_file.save("inventory_with_total_value.xlsx")
 
 
