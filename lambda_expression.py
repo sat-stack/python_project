43560c198b3d32254db4ffefc508781447125b02
sat-stack <satabdi.patra23@gmail.com>
2021-05-22 20:58:08 +0530

    Numpy_practice1

--- lambda_expression.py
+++ lambda_expression.py
@@ -1,26 +1,47 @@
-# lambda expression
+# n = int(input())
+# x = int(input())
+# y = int(input())
+# z = int(input())
 
-x = lambda a, b, c : a + b + c
-print(x(9, 12, 56))
-
-
-# triple
-
-def myfunc(n):
-  return lambda a : a * n
-
-mytripler = myfunc(8)
-
-print(mytripler(42))
+# print([[a, b, c] for a in range(0,x+1) for b in range(0, y+1) for c in range(0, z+1) if a + b + c != n])
 
 
-# double and triple
+# f = []
+# for i in range (0,x+1):
+#     for j in range (0,y+1):
+#         for k in range (0,z+1):
+#             if((i+j+k) is not n):
+#                 f.append([i,j,k])
+#
+#
+# print (f)
 
-def myfunc(n):
-  return lambda a : a * n
 
-mydoubler = myfunc(5)
-mytripler = myfunc(8)
+# if __name__ == '__main__':
+#     N = int(input())
+#     empty = []
+#     conv = []
+#
+#     for i in range(N):
+#
+#         x = input().split()
+#         empty.append(x)
+#
+#     for i in range(len(empty)):
+#         if empty[i][0] == 'insert':
+#             x = int(empty[i][1])
+#             y = int(empty[i][2])
+#             conv.insert(x, y)
+#         elif empty[i][0] == 'print':
+#             print(conv)
+#         elif empty[i][0] == 'remove':
+#             conv.remove(int(empty[i][1]))
+#         elif empty[i][0] == 'append':
+#             conv.append(int(empty[i][1]))
+#         elif empty[i][0] == 'sort':
+#             conv.sort()
+#         elif empty[i][0] == 'pop':
+#             conv.pop()
+#         elif empty[i][0] == 'reverse':
+#             conv.reverse()
 
-print(mydoubler(14))
-print(mytripler(14))
